eadea3ba27f756ff250262fe9d8dfb22bc516b73
Gill Abada <gill@callifi.com>
2019-03-01 12:22:18 -0500

    add routes.

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route, Link } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -29,8 +30,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route path="/smurf-form" render={props => <SmurfForm />} />
+        <Route path="/" render={props => <Smurfs {...props} smurfs={this.state.smurfs} /> } />
       </div>
     );
   }
